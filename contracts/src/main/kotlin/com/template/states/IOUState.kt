2d6060bafdab6630cae80459a341ec898627e5fd
apereverzin <andrey.pereverzin@hotmail.com>
2019-03-23 20:40:51 +0000

    Copied from cordapp-template-kotlin

---
+++ contracts/src/main/kotlin/com/template/states/IOUState.kt
@@ -0,0 +1,13 @@
+package com.template.states
+
+import com.template.contracts.TemplateContract
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.ContractState
+import net.corda.core.identity.Party
+
+@BelongsToContract(TemplateContract::class)
+class IOUState(val value: Int,
+               val lender: Party,
+               val borrower: Party) : ContractState {
+    override val participants get() = listOf(lender, borrower)
+}
