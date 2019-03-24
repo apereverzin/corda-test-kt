71cc010899c11ba1a68c4bc23146a25e59ef269b
apereverzin <andrey.pereverzin@hotmail.com>
2019-03-24 22:16:37 +0000

    Updated contract and flow

--- contracts/src/main/kotlin/com/template/states/IOUState.kt
+++ contracts/src/main/kotlin/com/template/states/IOUState.kt
@@ -1,11 +1,11 @@
 package com.template.states
 
-import com.template.contracts.TemplateContract
+import com.template.contracts.IOUContract
 import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.Party
 
-@BelongsToContract(TemplateContract::class)
+@BelongsToContract(IOUContract::class)
 class IOUState(val value: Int,
                val lender: Party,
                val borrower: Party) : ContractState {
--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -1,11 +1,13 @@
 package com.template.flows
 
 import co.paralleluniverse.fibers.Suspendable
-import com.template.contracts.TemplateContract
+import com.template.contracts.IOUContract
 import com.template.states.IOUState
 import net.corda.core.contracts.Command
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 
@@ -25,28 +27,44 @@ class IOUFlow(val iouValue: Int,
 
         // We create the transaction components.
         val outputState = IOUState(iouValue, ourIdentity, otherParty)
-        val command = Command(TemplateContract.Commands.Action(), ourIdentity.owningKey)
+        val command = Command(IOUContract.Create(), listOf(ourIdentity.owningKey, otherParty.owningKey))
 
         // We create a transaction builder and add the components.
         val txBuilder = TransactionBuilder(notary = notary)
-                .addOutputState(outputState, TemplateContract.ID)
+                .addOutputState(outputState, IOUContract.ID)
                 .addCommand(command)
 
-        // We sign the transaction.
+        // Verifying the transaction.
+        txBuilder.verify(serviceHub)
+
+        // Signing the transaction.
         val signedTx = serviceHub.signInitialTransaction(txBuilder)
 
         // Creating a session with the other party.
         val otherPartySession = initiateFlow(otherParty)
 
-        // We finalise the transaction and then send it to the counterparty.
-        subFlow(FinalityFlow(signedTx, otherPartySession))
-    }
+        // Obtaining the counterparty's signature.
+        val fullySignedTx = subFlow(CollectSignaturesFlow(signedTx, listOf(otherPartySession), CollectSignaturesFlow.tracker()))
+
+        // Finalising the transaction.
+        subFlow(FinalityFlow(fullySignedTx, otherPartySession))    }
 }
 
 @InitiatedBy(IOUFlow::class)
 class IOUFlowResponder(private val otherPartySession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
-        subFlow(ReceiveFinalityFlow(otherPartySession))
+        val signTransactionFlow = object : SignTransactionFlow(otherPartySession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+                "This must be an IOU transaction." using (output is IOUState)
+                val iou = output as IOUState
+                "The IOU's value can't be too high." using (iou.value < 100)
+            }
+        }
+
+        val expectedTxId = subFlow(signTransactionFlow).id
+
+        subFlow(ReceiveFinalityFlow(otherPartySession, expectedTxId))
     }
 }
